a109c6bc3a0054bcf29381c5d473d99a7cfa2b8c
jaewon0913 <jaewon9937@gmail.com>
2021-03-27 17:38:29 +0900

    싱글 파일 컴포넌트

---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+      <p>###########[S]AppHeader###########</p>
+      <h1>{{ propsdata }}</h1>
+      <button v-on:click="sendEvent">send</button>
+      <p>###########[E]AppHeader###########</p>
+  </header>
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+    methods: {
+        sendEvent: function name() {
+            this.$emit('renew')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
