cb07c6941114ce37cef8eb060f0ebac574245bff
jaewon0913 <jaewon9937@gmail.com>
2021-03-27 17:00:15 +0900

    vue cli 프로젝트 생성방법 및 설명

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,28 @@
+var appHeader => {
+    template: '<div>header</div>',
+    methods: {
+        addNum: function(){
+
+        }
+    }
+}
+
+<template>
+  <!-- HTML -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+    // Java Script
+    methods: function(){
+        addNum: function name(params) {
+            
+        }
+    }
+}
+</script>
+
+<style>
+    /* CSS */
+</style>
